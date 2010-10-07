83549021c429cab02e2799f27b899aa9a55af210
Krzysztof Kozmic <krzysztof@kozmic.pl>
2010-10-07 22:54:20 +1000

    - actual service for the controllers are the classes themselves so removed using IController as service.
    - in Windsor 2.5 component name is case insensitive so we don't need to call ToLower when registering...

--- Mike.MefAreas.Web/Installers/ControllersInstaller.cs
+++ Mike.MefAreas.Web/Installers/ControllersInstaller.cs
@@ -13,9 +13,8 @@ namespace Mike.MefAreas.Web.Installers
                 AllTypes
                     .FromThisAssembly()
                     .BasedOn<IController>()
-                    .WithService.Base()
                     .Configure(c => c.LifeStyle.Transient
-                                        .Named(c.Implementation.Name.ToLower()))
+                                        .Named(c.Implementation.Name))
                 );
         }
     }

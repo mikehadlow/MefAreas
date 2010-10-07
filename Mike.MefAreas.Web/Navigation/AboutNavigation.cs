3b9355f5e6878914c4269f3a5811b5974097838a
Krzysztof Kozmic <krzysztof@kozmic.pl>
2010-10-07 22:57:51 +1000

    - structural changes

---
+++ Mike.MefAreas.Web/Navigation/AboutNavigation.cs
@@ -0,0 +1,22 @@
+﻿using Mike.MefAreas.Core.Services;
+
+namespace Mike.MefAreas.Web.Navigation
+{
+    public class AboutNavigation : INavigation
+    {
+        public string Text
+        {
+            get { return "About"; }
+        }
+
+        public string Action
+        {
+            get { return "About"; }
+        }
+
+        public string Controller
+        {
+            get { return "Home"; }
+        }
+    }
+}
--- Mike.MefAreas.Web/Navigation/HomeNavigation.cs
+++ Mike.MefAreas.Web/Navigation/HomeNavigation.cs
@@ -19,22 +19,4 @@ namespace Mike.MefAreas.Web.Navigation
             get { return "Home"; }
         }
     }
-
-    public class AboutNavigation : INavigation
-    {
-        public string Text
-        {
-            get { return "About"; }
-        }
-
-        public string Action
-        {
-            get { return "About"; }
-        }
-
-        public string Controller
-        {
-            get { return "Home"; }
-        }
-    }
 }
